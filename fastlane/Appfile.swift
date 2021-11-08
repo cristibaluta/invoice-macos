5bdbac1b664e6610173e998e2767d9d5342c2563
Cristian Baluta <cristi.baluta@gmail.com>
2021-11-08 22:13:37 +0200

    Add fastlane beta

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "ro.imagin.Invoices" } // The bundle identifier of your app
+var appleID: String { return "cristi.baluta+apple@gmail.com" } // Your Apple email address
+
+var itcTeam: String? { return "298764" } // App Store Connect Team ID
+var teamID: String { return "5NHDC5EV44" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
